693194f5d37a9801af0e3cdc564f2a4f9218d7dd
Deniz Yuksekbukgu <fdenizyuksekbulgu@gmail.com>
2025-04-06 18:12:28 +0300

    updated

--- ShoppingApp2.Business/Data Protection/DataProtection.cs
+++ ShoppingApp2.Business/Data Protection/DataProtection.cs
@@ -28,19 +28,5 @@ namespace ShoppingApp2.Business.Data_Protection
     }
 }
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace ShoppingApp2.Business.Data_Protection
-//{
-//    public interface IDataProtection
-//    {
-//        string Protect(string text);
-//        string UnProtect(string protectedText);
-//    }
-//}
 
 
